0ef96d467f9601d15907e873199dbc52d0fb9425
Aydrian Howard <aydrian@gmail.com>
2023-10-22 10:30:32 -0400

    chore: initial tinacms set up

--- app/routes/blog+/$slug.tsx
+++ app/routes/blog+/$slug.tsx
@@ -1,50 +1,33 @@
 import { type LoaderFunctionArgs } from "@remix-run/cloudflare";
-import { useMemo } from "react";
 import { getParams } from "remix-params-helper";
 import {
   type TypedMetaFunction,
   typedjson,
   useTypedLoaderData
 } from "remix-typedjson";
+import client from "tina/__generated__/client";
+import { TinaMarkdown } from "tinacms/dist/rich-text";
 import z from "zod";
-
-import { Pre } from "~/components/mdx-components";
-import { getMDXComponent } from "~/utils/mdx";
 
 const ParamsSchema = z.object({
   slug: z.string()
 });
 
-type Frontmatter = {
-  [key: string]: any;
-};
-
-type BlogContentType = {
-  code: string;
-  frontmatter: Frontmatter;
-  hash: string;
-  html: string;
-  readTime: any;
-  slug: string;
-};
-
-export async function loader({ context, params }: LoaderFunctionArgs) {
+export async function loader({ params }: LoaderFunctionArgs) {
   const result = getParams(params, ParamsSchema);
   if (!result.success) {
     throw new Response(null, { status: 404, statusText: "Not Found" });
   }
   const slug = result.data.slug;
-  const env = context.env as Env;
 
-  const data = await env.CONTENT.get<BlogContentType>(`blog/${slug}`, "json");
-  if (!data) {
-    throw new Response(null, { status: 404, statusText: "Not Found" });
-  }
+  const postResponse = await client.queries.post({
+    relativePath: slug + ".mdx"
+  });
 
-  console.log({ frontmatter: JSON.stringify(data.frontmatter) });
+  const post = postResponse.data.post;
 
   return typedjson(
-    { ...data, slug }
+    { post }
     // { headers: { "cache-control": "max-age=3600000" } }
   );
 }
@@ -53,8 +36,8 @@ export const meta: TypedMetaFunction<typeof loader> = ({ data }) => {
   let title = "ItsAydrian Blog";
   let description = "";
   if (data) {
-    title = `${data.frontmatter.title} - ${title}`;
-    description = data.frontmatter.description;
+    title = data.post.title;
+    description = data.post.description ?? "";
   }
   return [
     { title },
@@ -102,28 +85,15 @@ export const meta: TypedMetaFunction<typeof loader> = ({ data }) => {
 };
 
 export default function BlogPost() {
-  const { code, frontmatter, html, readTime } =
-    useTypedLoaderData<typeof loader>();
-  const Component = useMemo(() => {
-    if (typeof window === "undefined" && !code) {
-      return null;
-    }
-    return getMDXComponent(code);
-  }, [code]);
-
+  const { post } = useTypedLoaderData();
   return (
     <>
       <header>
-        <h1>{frontmatter.title}</h1>
+        <h1>{post.title}</h1>
       </header>
       <main>
-        {Component ? (
-          <Component components={{ pre: Pre }} />
-        ) : (
-          <div dangerouslySetInnerHTML={{ __html: html }} />
-        )}
+        <TinaMarkdown content={post.body} />
       </main>
-      <div>{readTime?.text}</div>
     </>
   );
 }
--- remix.env.d.ts
+++ remix.env.d.ts
@@ -1,9 +1,3 @@
 /// <reference types="@remix-run/dev" />
 /// <reference types="@remix-run/cloudflare" />
 /// <reference types="@cloudflare/workers-types" />
-
-interface Env {
-  API_URL: string;
-  CONTENT: KVNamespace;
-  POST_API_KEY: string;
-}
