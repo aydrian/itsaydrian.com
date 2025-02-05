65e05232a2bd08b5f2cfcdadb0f03212e79aa8c6
Aydrian Howard <aydrian@gmail.com>
2025-02-05 11:23:29 -0500

    chore: migrated to React Router 7 with Hono

--- app/root.tsx
+++ app/root.tsx
@@ -1,32 +1,18 @@
-import type { LinksFunction } from "@remix-run/cloudflare";
+import type { LinksFunction } from "react-router";
+import { Links, Meta, Outlet, Scripts, ScrollRestoration } from "react-router";
+import styles from "~/styles/tailwind.css?url";
 
-import { cssBundleHref } from "@remix-run/css-bundle";
-import {
-  Links,
-  Meta,
-  Outlet,
-  Scripts,
-  ScrollRestoration
-} from "@remix-run/react";
-
-import iconHref from "~/components/icons/sprite.svg";
-
-import "./tailwind.css";
-
-export const links: LinksFunction = () => [
-  { as: "image", href: iconHref, rel: "preload", type: "image/svg+xml" },
-  { href: "/fonts/noto-sans-jp/font.css", rel: "stylesheet" },
-  ...(cssBundleHref ? [{ href: cssBundleHref, rel: "stylesheet" }] : [])
-];
+export const links: LinksFunction = () => [{ rel: "stylesheet", href: styles }];
 
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <head>
         <meta charSet="utf-8" />
-        <meta content="width=device-width, initial-scale=1" name="viewport" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
         <Meta />
         <Links />
+        <link rel="stylesheet" href={styles} />
       </head>
       <body>
         {children}
--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,16 @@
-import {
-  vitePlugin as remix,
-  cloudflareDevProxyVitePlugin as remixCloudflareDevProxy
-} from "@remix-run/dev";
-import { flatRoutes } from "remix-flat-routes";
+import { reactRouter } from "@react-router/dev/vite";
+import { cloudflareDevProxy } from "@react-router/dev/vite/cloudflare";
+import { reactRouterHonoServer } from "react-router-hono-server/dev";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   plugins: [
-    remixCloudflareDevProxy(),
-    remix({
-      future: {
-        v3_fetcherPersist: true,
-        v3_relativeSplatPath: true,
-        v3_throwAbortReason: true
-      },
-      routes: async (defineRoutes) => {
-        return flatRoutes("routes", defineRoutes);
-      }
+    cloudflareDevProxy(),
+    reactRouterHonoServer({
+      runtime: "cloudflare"
     }),
+    reactRouter(),
     tsconfigPaths()
   ]
 });
